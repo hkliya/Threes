fd3a8d984781e1c8729918aafe57f93dbf83fe65
hkliya <hkliya@gmail.com>
2014-03-08 23:32:31 +0800

    place an immovable cell when create the game

--- domain/src/main/java/com/seabornlee/threes/Game.java
+++ domain/src/main/java/com/seabornlee/threes/Game.java
@@ -1,7 +1,32 @@
 package com.seabornlee.threes;
 
+import java.util.Random;
+
 public class Game {
+    private static final int SIZE = 5;
+    private final Cell[][] matrix = new Cell[SIZE][SIZE];
     private boolean isRunning;
+
+    public Game() {
+        initMatrix();
+        placeAnImmovableCell();
+    }
+
+    private void initMatrix() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                matrix[i][j] = new Cell();
+            }
+        }
+    }
+
+    private void placeAnImmovableCell() {
+        Random random = new Random();
+        int row = random.nextInt(3);
+        int col = random.nextInt(3);
+
+        matrix[2 + row][2 + col] = Cell.IMMOVABLE;
+    }
 
     public boolean isStarted() {
         return isRunning;
@@ -9,5 +34,22 @@ public class Game {
 
     public void start() {
         isRunning = true;
+    }
+
+    public Cell cellAt(int row, int col) {
+        return matrix[row][col];
+    }
+
+    private static class Cell {
+        public static final Cell IMMOVABLE = new Cell() {
+            @Override
+            public boolean isImmovable() {
+                return true;
+            }
+        };
+
+        public boolean isImmovable() {
+            return false;
+        }
     }
 }
--- domain/src/test/groovy/GameTest.groovy
+++ domain/src/test/groovy/GameTest.groovy
@@ -9,4 +9,18 @@ class GameTest extends groovy.util.GroovyTestCase {
 
         assertTrue(game.isStarted())
     }
+
+    void testShouldInitWithOneImmovableCell() {
+        def game = new Game()
+
+        int countOfImmovableCell = 0
+        for (int i=0; i<5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (game.cellAt(i, j).isImmovable())
+                    countOfImmovableCell++
+            }
+        }
+
+        assertEquals(1, countOfImmovableCell)
+    }
 }
